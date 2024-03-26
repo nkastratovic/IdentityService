ffc20b77b2f8e1a6bb36b3a7101607acd7cb7eeb
nkastratovic <nkastratovic@gmail.com>
2024-03-26 17:56:09 +0100

    Add Authentication Authorization Services.

--- src/Identity.Service/Program.cs
+++ src/Identity.Service/Program.cs
@@ -1,3 +1,8 @@
+using Identity.Service.Data;
+using Identity.Service.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,25 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Identity
+builder.Services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
+{
+    options.Password.RequiredLength = 5;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireDigit = true;
+})
+ .AddEntityFrameworkStores<AppDbContext>()
+.AddDefaultTokenProviders()
+ .AddUserStore<UserStore<ApplicationUser, ApplicationRole, AppDbContext, Guid>>()
+ .AddRoleStore<RoleStore<ApplicationRole, AppDbContext, Guid>>()
+ ;
+
+builder.Services.AddAuthorization(options =>
+{
+});
 
 var app = builder.Build();
 
