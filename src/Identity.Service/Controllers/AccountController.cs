a9e631e922e23d748e90ceeaeafe721cbcd664d2
nkastratovic <nkastratovic@gmail.com>
2024-03-26 18:50:01 +0100

    Add Controller.

---
+++ src/Identity.Service/Controllers/AccountController.cs
@@ -0,0 +1,145 @@
+﻿using Identity.Service.DTO;
+using Identity.Service.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.Service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly RoleManager<ApplicationRole> _roleManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, RoleManager<ApplicationRole> roleManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="registrationRequestDto"></param>
+        /// <returns></returns>
+        [HttpPost("register")]
+        public async Task<ActionResult<ApplicationUser>> PostRegister(RegistrationRequestDto registrationRequestDto)
+        {
+            //Validation
+            if (ModelState.IsValid == false)
+            {
+                string errorMessage = string.Join(" | ", ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage));
+                return Problem(errorMessage);
+            }
+
+
+            //Create user
+            ApplicationUser user = new ApplicationUser()
+            {
+                Email = registrationRequestDto.Email,
+                PhoneNumber = registrationRequestDto.PhoneNumber,
+                UserName = registrationRequestDto.Email,
+                PersonName = registrationRequestDto.Name
+            };
+
+            IdentityResult result = await _userManager.CreateAsync(user, registrationRequestDto.Password);
+
+            if (result.Succeeded)
+            {
+                //sign-in
+                await _signInManager.SignInAsync(user, isPersistent: false);
+
+                //var authResponse = _jwtService.GenerateJwtToken(user);
+
+                //return Ok(authResponse);
+                return Ok();
+            }
+            else
+            {
+                string errorMessage = string.Join(" | ", result.Errors.Select(e => e.Description)); //error1 | error2
+                return Problem(errorMessage);
+            }
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IActionResult> IsEmailAlreadyRegistered(string email)
+        {
+            ApplicationUser? user = await _userManager.FindByEmailAsync(email);
+
+            if (user == null)
+            {
+                return Ok(true);
+            }
+            else
+            {
+                return Ok(false);
+            }
+        }
+
+        [AllowAnonymous]
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="loginRequestDto"></param>
+        /// <returns></returns>
+        [HttpPost("login")]
+        public async Task<IActionResult> PostLogin(LoginRequestDto loginRequestDto)
+        {
+            //Validation
+            if (ModelState.IsValid == false)
+            {
+                string errorMessage = string.Join(" | ", ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage));
+                return Problem(errorMessage);
+            }
+
+
+            var result = await _signInManager.PasswordSignInAsync(loginRequestDto.Email, loginRequestDto.Password, isPersistent: false, lockoutOnFailure: false);
+
+            if (result.Succeeded)
+            {
+                ApplicationUser? user = await _userManager.FindByEmailAsync(loginRequestDto.Email);
+
+                if (user == null)
+                {
+                    return NoContent();
+                }
+
+                //sign-in
+                await _signInManager.SignInAsync(user, isPersistent: false);
+
+                //var authenticationResponse = _jwtService.GenerateJwtToken(user);
+
+                //return Ok(authenticationResponse);
+                return Ok();
+            }
+
+            else
+            {
+                return Problem("Invalid email or password");
+            }
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("logout")]
+        public async Task<IActionResult> GetLogout()
+        {
+            await _signInManager.SignOutAsync();
+
+            return NoContent();
+        }
+    }
+}
