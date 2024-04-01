7624384c23a5ccbe86f891473b3aedd2cb22cb31
nkastratovic <nkastratovic@gmail.com>
2024-04-01 15:19:25 +0200

    Add TestApi service.

---
+++ src/TestApi/Controllers/AuthTestController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Get method");
+        }
+
+        [HttpGet]
+
+        [Route("{id:int}")]
+
+        public IActionResult Get(int id)
+        {
+            return Ok("Get by id method");
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "ADMIN, CUSTOMER")]
+        public IActionResult Post()
+        {
+            return Ok("Post method");
+        }
+
+        [HttpPut]
+        [Authorize(Roles = "ADMIN")]
+        public IActionResult Put()
+        {
+            return Ok("Put method");
+        }
+
+        [HttpDelete]
+        [Authorize(Roles = "ADMIN")]
+        public IActionResult Delete()
+        {
+            return Ok("Delete method");
+        }
+    }
+}
---
+++ src/TestApi/Program.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Get JWT Options
+JwtOptions jwtOptions = builder.Configuration.GetSection("ApiSettings:JwtOptions").Get<JwtOptions>();
+//Validate JWT Token
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidIssuer = jwtOptions.Issuer,
+        ValidAudience = jwtOptions.Audience,
+        IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(jwtOptions.Secret)),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ClockSkew = TimeSpan.Zero
+    };
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
